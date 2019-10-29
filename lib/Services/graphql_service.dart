52cca073219256c12a71605f35bd43193f458b2f
Taewan Kim <terry960302>
2019-10-30 02:34:16 +0900

    [수정]

--- lib/Services/graphql_service.dart
+++ lib/Services/graphql_service.dart
@@ -4,7 +4,7 @@ import 'package:graphql_flutter/graphql_flutter.dart';
 
 class GraphqlService {
 
-  static final HttpLink httpLink = HttpLink(uri: "http://14.40.12.135:4000/");
+  static final HttpLink httpLink = HttpLink(uri: "http://본인 ip주소:4000/");
   final ValueNotifier<GraphQLClient> client = ValueNotifier<GraphQLClient>(
     GraphQLClient(link: httpLink as Link, cache: OptimisticCache(
         dataIdFromObject: typenameDataIdFromObject))
